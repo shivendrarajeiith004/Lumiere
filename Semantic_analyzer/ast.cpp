00bce3ec6b0182d97c84d82aa3cb1f98c1a889ad
Jitik12 <shaikarmaan2004@gmail.com>
2024-11-11 11:40:21 +0530

    Armaan : DS for AST

--- Semantic_analyzer/ast.cpp
+++ Semantic_analyzer/ast.cpp
@@ -2,16 +2,35 @@
 
 class Code_Block{};
 
-class Asgn_Block : public Code_Block{
+class Conditional_Block : public Code_Block{
 };
 
-class Expr_Block : public Code_Block{
+class Expression_Block : public Code_Block {
+};
+
+class Reserved_Block : public Code_Block {
 };
 
 template <typename T>
 class Node{
   T* curr;
   T* next;
+
+  Node(){
+    curr = nullptr;
+    next = nullptr;
+  }
+
+  Node(T* new_curr){
+    curr = new_curr;
+    next = nullptr;
+  }
+
+  Node(T* new_curr, T* new_next){
+    curr = new_curr;
+    next = new_next;
+  }
+
 };
 
 template <typename T>
@@ -20,13 +39,23 @@ public:
   Node<T>* head;
   Node<T>* tail;
 
+  Linked_List(){
+    head = nullptr;
+    tail = nullptr;
+  }
+
   void push_back(T new_block){
+    if (new_block == nullptr){
+      std::cout << "Can not read a NULL block" << std::endl;
+      return;
+    }
     Node<T>* new_node = new Node<T>;
     new_node->curr = new_block;
     if(head == nullptr){
       head = new_node;
       tail = new_node;
-    }else{
+    }
+    else{
       tail->next = new_node;
       tail = new_node;
     }
@@ -34,8 +63,9 @@ public:
 };
 
 typedef struct Start{
-  Linked_List<Asgn_Block> asgn_blocks;
-  Linked_List<Expr_Block> decl_blocks;
+  Linked_List<Expression_Block> decl_blocks;
+  Linked_List<Conditional_Block> cond_blocks;
+  Linked_List<Reserved_Block> reserved_blocks;
 };
 
 int main(){
