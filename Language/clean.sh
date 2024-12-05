73f36b4ce4e3943506ba8a81850f932d1878c93a
shivendrarajeiith004 <shivendraraje004@gmail.com>
2024-12-05 16:30:48 +0530

    build/clean

---
+++ Language/clean.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define the list of folders
+folders=("Phase1-Tokens" "Phase2-3-Parsing_Syntax" "Phase4-Semantics")
+
+# Loop over each folder
+for folder in "${folders[@]}"; do
+  # Check if the src directory exists in the current folder
+  if [ -d "$folder/src" ]; then
+    echo "Processing $folder/src..."
+
+    # Navigate to the src directory
+    cd "$folder/src"
+
+    echo "Running 'make clean' in $folder/src"
+    make clean
+
+    # Return to the previous directory
+    cd - || exit
+  else
+    echo "Source directory not found in $folder"
+  fi
+done
+
+echo "All tasks cleaned."
